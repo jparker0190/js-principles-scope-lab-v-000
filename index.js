d447332b8463917115cbf2a8c81aabf8d6633669
Joseph Parker <jparker010190@gmail.com>
2018-07-07 16:55:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your solution in this file!
 var customerName = `bob`
 function upperCaseCustomerName(){
-var customerName = 'bob'
   customerName.toUpperCase()
   console.log(customerName)
 }
